1f9f44afe72e51405b48978ef2ea1080b1df3d27
Ayush Chandil <74442358+ayush-chandil@users.noreply.github.com>
2021-11-13 01:06:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Game from '../src/components/Game';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Game.js
@@ -0,0 +1,136 @@
+import React, { Component } from 'react'
+import './style.css';
+import Start from '../images/start1.png';
+
+
+
+const Winner = (first, second) => {
+  if (first === second) {
+    return "Tie"
+  }
+
+  switch (first) {
+    case "Rock":
+      if (second === "Paper")
+        return second;
+      else return first;
+
+    case "Paper":
+      if (second === "Scissors")
+        return second;
+      else return first;
+
+    case "Scissors":
+      if (second === "Rock")
+        return second;
+      else return first;
+  }
+
+}
+
+
+
+class Game extends Component {
+
+
+  state = {
+    Started: false,
+    Player: null,
+    Computer: null
+  };
+
+  render() {
+    const { Started, Player, Computer } = this.state;
+
+    const Images = {
+      Rock: "https://i.imgur.com/TONXH9s.png",
+      Paper: "https://i.imgur.com/t2154qR.png",
+      Scissors: "https://i.imgur.com/SXstPKk.png"
+    };
+
+    return (
+      <div>
+        <h1>Rock Paper Scissors</h1>
+
+        {Started ? (
+          <div className="game">
+            
+            <div className="player1"><p id="play">Player</p>
+              {Player ? (<img src={Images[Player]} />) : (
+                <div className="choose">
+
+                  {Object.keys(Images).map((a) => (
+                    <span key={a} onClick={() => {
+                      this.setState({
+                        Player: a,
+                        Computer: Object.keys(Images)[
+                          Math.floor(Math.random() * Object.keys(Images).length)
+                        ]
+                      }
+                      );
+                      
+                    }}>
+
+                      <img src={Images[a]} alt={a} />
+                      {a}
+                    </span>
+                  ))}
+                </div>
+              )}
+            </div>
+
+
+
+            <div className="player2"><p id="comp">Computer</p>
+                
+              {Computer ? (<img src={Images[Computer]} />) : (
+                <img src="https://i.imgur.com/CyvHqQH.png" />
+              )}
+            </div>
+
+          </div>
+
+        ) : (
+          <img src={Start} id="start" alt="Start" onClick={() => {
+            this.setState({ Started: true });
+          }} />
+        )}
+
+        {Player && Computer &&
+          <p className="result">
+
+            {(() => {
+              const Win = Winner(Player, Computer);
+              if (Win === "Tie") {
+                return "Nobody Wins!";
+              }
+              else {
+                if (Win === Player) {
+                  return "Player Wins!";
+                } else {
+                  return "Computer Wins!";
+                }
+              }
+            })()}
+
+
+
+            <img id="restart" onClick={() => this.setState({
+              Started: true,
+              Player: null,
+              Computer: null
+            })} src="https://static.thenounproject.com/png/904768-200.png" /></p>}
+
+           
+
+      </div>
+
+
+    )
+
+  }
+
+}
+
+export default Game
+
